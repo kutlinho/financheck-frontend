ebdab678287f2cc7f87e59f9b07a187b2bc7453e
Kutluhan Meydanlı <kutlumeydanli@gmail.com>
2026-01-19 21:26:19 +0300

    (feat) custom card added and connected to login page

---
+++ src/components/CustomCard/CustomCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import clsx from "clsx";
+
+const CustomCard = ({
+                    header,
+                    children,
+                    width = "w-full",
+                    height = "h-auto",
+                    className = "",
+                    headerClassName = "",
+                    bodyClassName = "",
+                }) => {
+    return (
+        <div
+            className={clsx(
+                "rounded-4xl border-3 border-primary-green-light bg-neutral-bg-grey shadow-sm",
+                width,
+                height,
+                className
+            )}
+        >
+            {header && (
+                <div
+                    className={clsx(
+                        "px-4 py-3 text-3xl font-semibold text-gray-800",
+                        headerClassName
+                    )}
+                >
+                    {header}
+                </div>
+            )}
+
+            <div className={clsx("p-4 text-center", bodyClassName)}>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default CustomCard;
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import CustomCard from "../../components/CustomCard/CustomCard.jsx";
 
 function Login() {
     return (
-        <div>Login</div>
+        <div className="flex justify-center align-center h-full w-full">
+        <CustomCard
+            bodyClassName="font-300"
+        headerClassName="text-center pt-16"
+        header="Hesabınıza Giriş Yapın"
+        width="w-[753px]"
+        height="h-[545px]"
+        >
+        Uygulamaya giriş yapabilmek için kullanıcı adı ve şifrenizi girin
+        </CustomCard>
+        </div>
     );
 }
 
