8832555bafdfc792a835a6e69d43532c40ad7a8d
Kutluhan Meydanlı <kutlumeydanli@gmail.com>
2026-03-01 01:12:56 +0300

    (feat,style) Register components added to Login component

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -6,9 +6,18 @@ import CustomButton from "../../components/CustomButton/CustomButton.jsx";
 import {FcGoogle} from "react-icons/fc";
 
 
-function Login() {
-    const usernameRef = useRef(null);
+function Login({isRegister}) {
+    const firstNameInputRef = useRef(null);
+    const lastNameInputRef = useRef(null);
+    const emailRef = useRef(null);
     const passwordRef = useRef(null);
+    const passwordCheckRef = useRef(null);
+
+
+    const customHeader = isRegister ? "Financheck’e Kaydolun" : "Hesabınıza Giriş Yapın"
+    const cardText = isRegister ? "Uygulamayı kullanabilmek için aşağıdaki bilgileri doldurarak kaydolun."
+        : "Uygulamaya giriş yapabilmek için kullanıcı adı ve şifrenizi girin"
+    const buttonLabel = isRegister ? "Kaydolun" : "Giriş Yapın"
 
     return (<div className="flex flex-col justify-center items-center gap-5 h-screen w-full">
 
@@ -16,15 +25,27 @@ function Login() {
         <CustomCard
             bodyClassName="flex flex-col items-center font-300 gap-5"
             headerClassName="text-center pt-16"
-            header="Hesabınıza Giriş Yapın"
+            header={customHeader}
             width="w-[753px]"
             height="h-[545px]"
         >
-            Uygulamaya giriş yapabilmek için kullanıcı adı ve şifrenizi girin
+            {cardText}
+
+            {isRegister && <InputText
+                ref={firstNameInputRef}
+                placeholder="İsim"
+                className="w-[327px]"
+            />}
+
+            {isRegister && <InputText
+                ref={lastNameInputRef}
+                placeholder="Soyisim"
+                className="w-[327px]"
+            />}
 
             <InputText
-                ref={usernameRef}
-                placeholder="Kullanıcı Adı"
+                ref={emailRef}
+                placeholder="Email"
                 className="w-[327px]"
             />
 
@@ -34,20 +55,34 @@ function Login() {
                 placeholder="Şifre"
                 className="w-[327px]"
             />
-            <CustomButton className="w-[327px]" type="primary" label="Giriş Yapın"/>
+
+            {isRegister && <InputText
+                ref={passwordCheckRef}
+                type="password"
+                placeholder="Şifre Tekrar"
+                className="w-[327px]"
+            />}
+
+            <CustomButton className="w-[327px]" type="primary" label={buttonLabel}/>
 
             <div className="flex items-center w-[327px]">
                 <div className="border-t border-gray-300 min-w-12 w-36"></div>
-                    <span className="mx-4 text-sm text-gray-500 font-medium">
+                <span className="mx-4 text-sm text-gray-500 font-medium">
                         veya
                     </span>
                 <div className="border-t border-gray-300 min-w-12 w-36"></div>
             </div>
 
-            <CustomButton className="w-[327px]" type="alternative" label="Google ile Giriş Yapın" icon={<FcGoogle />} />
+            <CustomButton className="w-[327px]" type="alternative" label="Google ile Giriş Yapın" icon={<FcGoogle/>}/>
 
-            <p className="text-sm">Hesabınız yok mu? <a href="/register" target="_blank" className="text-black hover:underline">Kaydolun</a>
-            </p>
+            {isRegister ?
+                <p className="text-sm">Mevcut Hesabınız var mı? <a href="/login" target="_blank"
+                                                                   className="text-black hover:underline">Giriş
+                    Yapın</a></p>
+                : <p className="text-sm">Hesabınız yok mu? <a href="/register" target="_blank"
+                                                              className="text-black hover:underline">Kaydolun</a>
+                </p>
+            }
         </CustomCard>
 
     </div>);
